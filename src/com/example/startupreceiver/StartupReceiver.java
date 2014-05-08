477b6520d6d257919584e090db7a09b339d2d185
Bootez <8ootez@gmail.com>
2014-05-08 17:05:36 +0800

    Starup after boot successfully

---
+++ src/com/example/startupreceiver/StartupReceiver.java
@@ -0,0 +1,17 @@
+package com.example.startupreceiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class StartupReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Intent i = new Intent(context, MainActivity.class);
+        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        //Send i to OS
+        context.startActivity(i);
+    }
+
+}
